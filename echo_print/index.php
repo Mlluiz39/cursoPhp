7853ba412b9f79d470f4d47ee93c12037fbe7895
Marcelo <mlluizpereira39@gmail.com>
2021-07-19 17:51:13 -0300

    comandos echo e print

---
+++ echo_print/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Echo e print</title>
+</head>
+<body>
+  <h1>Echo e print</h1>
+
+  <?php
+
+  echo 'comando echo </br>';
+  
+  print 'comando print<hr>';
+  ?>
+  
+</body>
+</html>
---
+++ embuntind_ bloco_PHP_in_HTML/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Incluindo PHP no html</title>
+</head>
+
+<body>
+  <h1>Incluindo PHP no HTML</h1>
+
+  <?php
+  echo 'utlizando a tag padrão';
+  ?>
+  <hr>
+  <?=
+  'utilizando a tag de impressão';
+  ?>
+</body>
+
+</html>
