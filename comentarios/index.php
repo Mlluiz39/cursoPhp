ae343310595094cacb76a31a4b40007128f90e1b
Marcelo <mlluizpereira39@gmail.com>
2021-07-19 17:57:11 -0300

    comentários php

---
+++ comentarios/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Comentarios PHP</h1>
+  <!-- comentários html-->
+
+  <?php
+   echo 'Comentarios de uma linha';
+  //comentários de uma linha
+  echo 'Comentários de uma linha via shell';
+  #comentários de uma linha estilo shell
+  echo 'Comentarios de multiplas linhas'
+  /*
+  comentários de múltiplas linhas
+  aqui é outra linha
+  */
+  
+  
+  ?>
+  
+</body>
+</html>
