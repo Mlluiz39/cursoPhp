4220a3fafc70f50f09f401cf315a9cd74d09ae32
Mlluiz39 <mlluizpereira39@gmail.com>
2021-08-02 08:48:25 -0300

    add jason do projeto

--- embuntindo bloco PHP em HTML/index.php
+++ embuntindo bloco PHP em HTML/index.php
@@ -1,22 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Incluindo php no html</title>
+</head>
+<body>
+  <h1>Incluindo php no html</h1>
 
-class Main
-{
-  public function __construct()
-  {
-    echo "Hello\n";
-  }
-  /**
-   * just a test
-   *
-   * @param string $name
-   * @return void
-   */
-  public function teste(string $name): void
-  {
-    
-  }
-}
-
-
-(new Main())->teste('Marcelo');
+  <?php 
+  echo 'utlizando tag padrão';
+  ?>
+</body>
+</html>
