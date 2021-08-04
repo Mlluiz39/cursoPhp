e419b060168b392c48058b6b4bd71f3a732493ed
Mlluiz39 <mlluizpereira39@gmail.com>
2021-08-04 18:38:17 -0300

    operadores com atribuição

---
+++ operadores_atribuicao_aritmeticos/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Operadores com atribuição</title>
+</head>
+<body>
+  <h1>Operadores aritmeticos com operador de atribuição</h1>
+
+  <?php 
+    $x =10;
+    $y = 8;
+
+    $x = $x + 5;
+
+    echo $x;
+
+    $x += 12;
+    echo '<br>';
+
+    echo $x;
+
+    $x -= 6;
+    echo '<br>';
+
+    echo $x;
+
+    $x %= $y;
+    echo '<br>';
+
+    echo $x;
+  
+  
+  ?>
+  
+</body>
+</html>
