bda746c0db1f5d5e8c9d6f1e5f486b0efea9fad8
Mlluiz39 <mlluizpereira39@gmail.com>
2021-08-04 18:28:28 -0300

    operadores aritmeticos

---
+++ operadores_aritimeticos/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Operadores aritmeticos</title>
+</head>
+<body>
+  <h1>Operadores aritmeticos</h1>
+
+  <?php
+
+  $num1 = 13;
+  $num2 = 4;
+
+  echo "A soma entre $num1 e $num2 = " . ($num1 + $num2);
+  echo '<br><br>';
+  echo "A subtração entre $num1 e $num2 = " . ($num1 - $num2);
+  echo '<br><br>';
+  echo "A multiplicação entre $num1 e $num2 = " . ($num1 * $num2);
+  echo '<br><br>';
+  echo "A divisão entre $num1 e $num2 = " . ($num1 / $num2);
+  echo '<br><br>';
+  echo "O resto da divisão entre $num1 e $num2 = " . ($num1 % $num2);
+
+
+
+?>
+  
+</body>
+</html>
